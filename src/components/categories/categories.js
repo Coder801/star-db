67ee750a57ba804567f2236aeb460b6b920a1614
Dmitriy Onischenko <coder@zeoalliance.com>
2019-10-25 20:07:02 +0300

    WIP

--- src/components/categories/categories.js
+++ src/components/categories/categories.js
@@ -5,16 +5,21 @@ import style from "./style.module.scss";
 import { API_IMAGE_BASE, CATEGORIES } from "../../constants";
 import { joinImagePathUrl } from "../../helpers";
 
+import img1 from "./img/characters.jpg";
+import img2 from "./img/films.jpg";
+import img3 from "./img/species.jpg";
+import img4 from "./img/starships.jpg";
+import img5 from "./img/vehicles.jpg";
+import img6 from "./img/planets.jpg";
+
+const images = [img1, img2, img3, img4, img5, img6];
+
 const renderList = list =>
   Object.values(list).map(({ label, route, categoryImageUrl }, key) => (
     <li className={style.item} key={key}>
       <Link className={style.link} to={route}>
         <figure className={style.figure}>
-          <img
-            className={style.image}
-            src={joinImagePathUrl.jpg(API_IMAGE_BASE, "categories", categoryImageUrl)}
-            alt={label}
-          />
+          <img className={style.image} src={images[key]} alt={label} />
           <figcaption className={style.caption}>{label}</figcaption>
         </figure>
       </Link>
