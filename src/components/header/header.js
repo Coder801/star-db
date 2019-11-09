499abc25148fa6fd58b21c7693ab3f7ba5282ce2
Dmitriy Onischenko <coder@zeoalliance.com>
2019-11-09 21:59:45 +0200

    [WIP]

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { SwSearch } from "../sw-components";
 import Navigation from "../navigation";
+import Image from "../../components/image";
 
 import logo from "./img/logo-header.png";
 
@@ -10,19 +11,35 @@ import { CATEGORIES as categories } from "../../constants";
 
 const Header = () => {
   return (
-    <div className={`${style.header} ${style.navOpen}`}>
-      <nav className={style.nav}>
-        <Navigation categories={categories} />
-      </nav>
-      <div className={style.logo}>
-        <Link className={style.link} to="/">
-          <img className={style.image} src={logo} alt="" />
-        </Link>
+    <Fragment>
+      <div className={`${style.header} ${style.navOpen}`}>
+        <nav className={style.nav}>
+          <Navigation categories={categories} />
+        </nav>
+        <div className={style.logo}>
+          <Link className={style.link} to="/">
+            <img className={style.image} src={logo} alt="" />
+          </Link>
+        </div>
+        <div className={style.search}>
+          <SwSearch />
+        </div>
       </div>
-      <div className={style.search}>
-        <SwSearch />
+      {/* For test start */}
+      <div className={style.searchResults}>
+        <ul className={style.list}>
+          <li className={style.item}>
+            <figure className={style.thumbnail}>
+              <div className={style.image}>
+                <Image src={"./"} alt="" />
+              </div>
+            </figure>
+            test
+          </li>
+        </ul>
       </div>
-    </div>
+      {/* For test End */}
+    </Fragment>
   );
 };
 
--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -37,7 +37,7 @@ const data = [
 
 // Some comment for test commit
 
-const SearchResults = ({ data }) => {
+const Results = ({ data }) => {
   const results = data => {
     return data.map(({ name, img, category }, key) => {
       return (
@@ -52,30 +52,26 @@ const SearchResults = ({ data }) => {
     });
   };
 
-  return <ul className={style.list}>{results(data)}</ul>;
-};
-
-const SearchField = ({ value }) => {
-  const [state, setState] = useState({
-    value
-  });
-
-  const onChange = event => {
-    const { value } = event.currentTarget;
-    setState({ value });
-  };
-
   return (
-    <div className={style.searchField}>
-      <input value={state.value} onChange={onChange} />
+    <div className={style.results}>
+      <ul className={style.list}>{results(data)}</ul>
     </div>
   );
 };
 
+const NoResults = () => {
+  return (
+    <div className={style.noResults}>
+      <h2>No Results</h2>
+    </div>
+  );
+};
+
+const SearchResults = ({ data }) => (data ? <Results data={data} /> : <NoResults />);
+
 const Search = ({ search }) => {
   return (
     <div className={style.page}>
-      <SearchField value="test" />
       <SearchResults data={data} />
     </div>
   );
